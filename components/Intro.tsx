9d46502fa23310a6c82ea83a1a168e5ebd44e6b7
Robert Renzo Rudio <robertrenzorudio@gmail.com>
2022-01-12 18:30:28 -0800

    Fix emoji rendering on intro

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,15 +1,20 @@
+import GraphemeSplitter from 'grapheme-splitter';
 import Typewriter from 'typewriter-effect';
 
 interface Props {}
 
 const Intro = (props: Props) => {
+  const stringSplitter: any = (str: string) => {
+    const splitter = new GraphemeSplitter();
+    return splitter.splitGraphemes(str);
+  };
   return (
     <div className="flex justify-between mx-auto px-6 md:px-12 lg:px-0 lg:max-w-3xl 2xl:max-w-5xl">
       <div className="w-11/12 lg:w-4/5 flex flex-col text-blue-1100 dark:text-indigo-100 space-y-3 ">
         <span className="text-lg font-light">Hey there, I am</span>
         <span className="text-3xl md:text-5xl text-rose-600 dark:text-orange-500 font-extrabold">
           <Typewriter
-            options={{ delay: 50 }}
+            options={{ delay: 50, stringSplitter }}
             onInit={(typewriter) =>
               typewriter.typeString('Robert Renzo Rudio 👋 ').start()
             }
