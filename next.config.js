f0e56a7aa318e3bed0ed27e19c939a945f579d25
Robert Renzo Rudio <robertrenzorudio@gmail.com>
2022-03-09 17:38:42 -0800

    Add image domain

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    domains: ['static.rudio.dev'],
+    domains: ['static.rudio.dev', 'd2dgspc4qdr5xo.cloudfront.net'],
   },
   reactStrictMode: true,
 };
